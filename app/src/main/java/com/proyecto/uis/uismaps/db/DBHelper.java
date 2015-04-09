ebf79837d04cb2f1f6be6eb98610c3eada5eab72
cheloreyes <cheloreyes@gmail.com>
2015-04-09 06:44:21 -0500

    Data base compacted. Se reduce de manera significativa el tamaño de la aplicación.

--- app/src/main/java/com/proyecto/uis/uismaps/db/DBHelper.java
+++ app/src/main/java/com/proyecto/uis/uismaps/db/DBHelper.java
@@ -140,7 +140,7 @@ public class DBHelper extends SQLiteOpenHelper implements Constants {
 
     public Bitmap imageBuilding(String building) {
         byte[] image = new byte[1024];
-        Cursor c = iDataBase.rawQuery("SELECT Image FROM Edifice WHERE EdificeName='" + building + "'", null);
+        Cursor c = iDataBase.rawQuery("SELECT Image FROM Edifice WHERE EdificeName LIKE '%" + building + "%'", null);
         if(c != null) {
             while (c.moveToNext()){
                 if(c.getBlob(0) != null) image = c.getBlob(0);
